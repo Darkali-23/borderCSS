bc0e540eaa48b20b1797ff170db97cbdebabf499
Darkali-23 <syed0786@gmail.com>
2021-12-06 20:38:45 +0530

    Oauth is done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-// require('dotenv').config();
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -8,7 +8,11 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const path = require("path");
-const pathview = path.join(__dirname, "/views")
+const pathview = path.join(__dirname, "/views");
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const { env } = require("process");
+const findOrCreate = require('mongoose-findorcreate');
+const { strictEqual } = require('assert');
 
 
 
@@ -34,17 +38,38 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("user", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        passReqToCallback: true
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+            return done(err, user);
+        });
+    }
+));
 
 
 // get method
@@ -53,6 +78,16 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
+        successRedirect: "/secrets",
+        failureRedirect: "/register"
+    }));
+
 app.get("/login", function(req, res) {
     res.render("login");
 });
@@ -60,9 +95,9 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
     res.render("register");
 });
-app.get("/sec", function(req, res) {
+app.get("/secrets", function(req, res) {
     if (req.isAuthenticated) {
-        res.render("/sec");
+        res.render("/secrets");
     } else {
         res.redirect("/login");
     }
@@ -100,7 +135,7 @@ app.post("/login", function(req, res) {
             res.render("/login");
         } else {
             passport.authenticate("local")(req, res, function() {
-                res.render("/sec");
+                res.render("/secrets");
             });
         }
     });
