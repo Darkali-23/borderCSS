1da57ccd164c214aef3107fb859a8be5442a8f8c
Darkali-23 <syed0786@gmail.com>
2021-12-06 19:42:58 +0530

    error in loading secrets pages

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 //jshint esversion:6
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-
-mongoose.connect("mongodb://localhost:27017/userDB");
-
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-
-const User = mongoose.model("user", userSchema);
-
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const path = require("path");
+const pathview = path.join(__dirname, "/views")
 
 
 
@@ -26,7 +16,36 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("views", pathview);
 app.set("view engine", "ejs");
+app.use(session({
+    secret: "This is a secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+// mongoose.set("useCreateIndex", true);
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+
+const User = mongoose.model("user", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // get method
 
@@ -41,53 +60,52 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
     res.render("register");
 });
+app.get("/sec", function(req, res) {
+    if (req.isAuthenticated) {
+        res.render("/sec");
+    } else {
+        res.redirect("/login");
+    }
+});
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 // Post Method
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/login");
+            });
+        }
     });
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        newUser.save(function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("login");
-            }
-        });
-    });
-
-
 });
 
 
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: username }, function(err, resultLogin) {
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
+            res.render("/login");
         } else {
-            if (resultLogin) {
-                bcrypt.compare(password, resultLogin.password, function(err, result) {
-                    if (result == true) {
-                        res.render("secrets");
-                    } else {
-                        res.render("login");
-                    }
-                });
-
-            } else {
-                console.log("we couldn't find the user,sorry");
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.render("/sec");
+            });
         }
     });
-})
+
+});
 app.listen(3000, function(request, response) {
     console.log("Server has started successfully at the port 3000");
 });
